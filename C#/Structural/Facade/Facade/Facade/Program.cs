30853bfd765bba1e84264f63fb8724b4bb45da8a
Jac21 <jcantu521@gmail.com>
2016-06-17 11:13:27 -0500

    [C#] Added Facade pattern implementation

---
+++ C#/Structural/Facade/Facade/Facade/Program.cs
@@ -0,0 +1,47 @@
+﻿// Provide a unified interface to a set of interfaces in a subsystem. 
+// Façade defines a higher-level interface that makes the subsystem easier to use.
+
+using System;
+
+namespace Facade
+{
+    /*
+     * Facade   (MortgageApplication)
+        knows which subsystem classes are responsible for a request.
+        delegates client requests to appropriate subsystem objects.
+
+     * Subsystem classes   (Bank, Credit, Loan)
+        implement subsystem functionality.
+        handle work assigned by the Facade object.
+        have no knowledge of the facade and keep no reference to it.
+     */
+
+    class Program
+    {
+        static void Main()
+        {
+            /*
+             *  Structural implementation
+             */
+
+            // Facade
+            StructuralFacade.StructuralFacade structuralFacade = new StructuralFacade.StructuralFacade();
+
+            structuralFacade.MethodA();
+            structuralFacade.MethodB();
+
+            /*
+             *  Real-World implementation
+             */
+
+            // Facade
+            RealWorldFacade.RealWorldFacade realWorldFacade = new RealWorldFacade.RealWorldFacade();
+
+            // evaluate mortage eligibility for customer
+            RealWorldFacade.Customer customer = new RealWorldFacade.Customer("Jeremy Cantu");
+            bool eligible = realWorldFacade.IsEligible(customer, 100000);
+
+            Console.WriteLine("\n" + customer.Name + " has been " + (eligible ? "Approved" : "Rejected"));
+        }
+    }
+}
---
+++ C#/Structural/Facade/Facade/Facade/StructuralFacade/StructuralFacade.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+// provides a simplified and uniform interface to a large subsystem of classes.
+namespace Facade.StructuralFacade
+{
+    class SubSystemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine("SubsystemOne Method");
+        }
+    }
+
+    class SubSystemTwo
+    {
+        public void MethodTwo()
+        {
+            Console.WriteLine("SubSystemTwo Method");
+        }
+    }
+
+    class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine("SubSystemThree Method");
+        }
+    }
+
+    class SubSystemFour
+    {
+        public void MethodFour()
+        {
+            Console.WriteLine("SubSystemFour Method");
+        }
+    } 
+
+    public class StructuralFacade
+    {
+        private SubSystemOne _one;
+        private SubSystemTwo _two;
+        private SubSystemThree _three;
+        private SubSystemFour _four;
+
+        public StructuralFacade()
+        {
+            _one = new SubSystemOne();
+            _two = new SubSystemTwo();
+            _three = new SubSystemThree();
+            _four = new SubSystemFour();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("\nMethodA() ---- ");
+            _one.MethodOne();
+            _two.MethodTwo();
+            _three.MethodThree();
+            _four.MethodFour();
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("\nMethodB() ---- ");
+            _two.MethodTwo();
+            _three.MethodThree();
+        }
+    }
+}
